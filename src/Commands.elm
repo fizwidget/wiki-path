3743be9df8098ea4116c83d9ba0840578df8cc71
James Russell <fizwidget@gmail.com>
2018-02-22 21:56:24 +1100

    Attempt to decode Wikipedia Json, still a work in progress

--- src/Commands.elm
+++ src/Commands.elm
@@ -1,6 +1,8 @@
 module Commands exposing (fetchArticle)
 
 import Jsonp
+import Json.Decode exposing (int, string, float, nullable, Decoder)
+import Json.Decode.Pipeline exposing (decode, required, optional, hardcoded)
 import Messages exposing (Message(FetchArticleResult))
 import Model exposing (Url)
 import RemoteData
@@ -17,3 +19,26 @@ fetchArticle articleTitle =
 getUrl : String -> String
 getUrl articleTitle =
     "https://en.wikipedia.org/w/api.php?action=query&titles=" ++ articleTitle ++ "&prop=revisions&rvprop=content&format=json&formatversion=2"
+
+
+type alias Article =
+    { title : String
+    , content : String
+    }
+
+
+type alias Pages =
+    { pages : List Article
+    }
+
+
+articleDecoder : Decoder Article
+articleDecoder =
+    decode Article
+        |> required "query" queryDecoder
+
+
+queryDecoder : Decoder Pages
+queryDecoder =
+    decode Pages
+        |> required "pages" Pages
