8d4c410d2666ff0a16c7d11930ae8b6b5fe3a930
James Russell <fizwidget@gmail.com>
2018-02-22 21:47:02 +1100

    Add Json decoder and get Jsonp library working

--- src/Commands.elm
+++ src/Commands.elm
@@ -6,8 +6,14 @@ import Model exposing (Url)
 import RemoteData
 
 
-fetchArticle : Url -> Cmd Message
-fetchArticle url =
-    Jsonp.getString url
+fetchArticle : String -> Cmd Message
+fetchArticle articleTitle =
+    getUrl articleTitle
+        |> Jsonp.get
         |> RemoteData.sendRequest
         |> Cmd.map FetchArticleResult
+
+
+getUrl : String -> String
+getUrl articleTitle =
+    "https://en.wikipedia.org/w/api.php?action=query&titles=" ++ articleTitle ++ "&prop=revisions&rvprop=content&format=json&formatversion=2"
