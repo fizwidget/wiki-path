b1141007f9a02b851e98893220c24bc95765e7df
James Russell <fizwidget@gmail.com>
2018-05-12 16:12:04 +1000

    Update build script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-printf "Checking out deployment branch...\n"
+printf "Checking out deployment branch...\n\n"
 git checkout gh-pages
 
-printf "Merging master into deployment branch\n"
+printf "Merging master into deployment branch\n\n"
 git merge master -m "Merging master"
 
-printf "Building...\n"
+printf "Building...\n\n"
 ./build.sh
 
-printf "Comitting new assets...\n"
+printf "Comitting new assets...\n\n"
 git commit -am "Updating build"
 
-printf "Pushing built result...\n"
+printf "Pushing built result...\n\n"
 git push -f
 
-printf "Checking out master...\n"
+printf "Checking out master...\n\n"
 git checkout master
 
 printf "Deployment successful (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧"
