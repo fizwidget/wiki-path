c49e855fd73d20b3496d6c4faf0e56610ef4a634
James Russell <fizwidget@gmail.com>
2018-05-13 14:53:01 +1000

    Replace 'Subscriptions' file with lambda function

--- src/Subscriptions.elm
+++
@@ -1,9 +0,0 @@
-module Subscriptions exposing (subscriptions)
-
-import Model exposing (Model)
-import Messages exposing (Msg)
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
