b78e445ead0bd23af6acaf40095ee340791fd6fa
James Russell <fizwidget@gmail.com>
2018-06-17 12:40:15 +1000

    Pressing 'Back' from finished phase will pre-popupate the inputs

--- src/Common/Path/Model.elm
+++ src/Common/Path/Model.elm
@@ -2,6 +2,8 @@ module Common.Path.Model
     exposing
         ( Path
         , beginningWith
+        , beginning
+        , end
         , inOrder
         , inReverseOrder
         , priority
@@ -22,6 +24,19 @@ beginningWith articleTitle =
         , previousStops = []
         , priority = 0
         }
+
+
+beginning : Path -> Title
+beginning (Path path) =
+    path.previousStops
+        |> List.reverse
+        |> List.head
+        |> Maybe.withDefault path.nextStop
+
+
+end : Path -> Title
+end (Path path) =
+    path.nextStop
 
 
 inOrder : Path -> List Title
--- src/Finished/Update.elm
+++ src/Finished/Update.elm
@@ -3,10 +3,19 @@ module Finished.Update exposing (update)
 import Setup.Init
 import Model exposing (Model)
 import Messages exposing (Msg)
+import Common.Path.Model as Path
+import Common.Title.Model as Title
 import Finished.Messages exposing (FinishedMsg(BackToSetup))
-import Finished.Model exposing (FinishedModel)
+import Finished.Model exposing (FinishedModel(Success, Error))
 
 
 update : FinishedMsg -> FinishedModel -> ( Model, Cmd Msg )
 update BackToSetup model =
-    Setup.Init.init
+    case model of
+        Success pathToDestination ->
+            Setup.Init.initWithInput
+                (Path.beginning pathToDestination |> Title.value)
+                (Path.end pathToDestination |> Title.value)
+
+        Error _ ->
+            Setup.Init.init
